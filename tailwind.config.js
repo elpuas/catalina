f3923efa27712ceea3b849ab2e40f85e4d031391
Alfredo Navas <elpuas@gmail.com>
2021-10-20 15:13:55 -0600

    add tailwind config viewer, update tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,28 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: '#5E6F00',
+      secondary: '#B6D600',
+      tertiary: '#D8FF00',
+    },
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+      interBold: ['Inter Bold', 'sans-serif'],
+      interBlack: ['Inter Black', 'sans-serif'],
+    },
+    fontSize: {
+      hero: '6rem',
+      heroMobile: '4.625rem',
+      sectionHeader: '2.625rem',
+      sectionHeaderMobile: '2.25rem',
+      sectionContent: '1.5rem',
+      base: '1.125rem',
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
