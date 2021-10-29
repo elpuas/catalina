d586b678a848e36967cd35dc4a9d38d6e252890d
Alfredo Navas <elpuas@gmail.com>
2021-10-29 10:11:23 -0600

    add react-datocms for structured text

--- src/components/blocks/Navigation/Navigation.js
+++ src/components/blocks/Navigation/Navigation.js
@@ -1,19 +1,43 @@
-import { Link } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 import { useState } from 'react';
 import * as React from 'react';
 import cx from 'classnames';
 import { Squash as Hamburger } from 'hamburger-react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import * as styles from './Navigation.module.css';
 import useMediaQuery from '../../../hooks/useMediaQuery';
 /**
  * The Navigation Component
  *
  * @author Alfredo Navas <elpuas@gmail.com>
- * @param {Array} menu The menu items array
  * @return {Element} Renders the Navigation
  */
-export default function Navigation({ menu }) {
+export default function Navigation() {
+  const data = useStaticQuery(graphql`
+    query Navigation {
+      allDatoCmsNavigation {
+        nodes {
+          model {
+            apiKey
+          }
+          menu {
+            ... on DatoCmsFrontPage {
+              id
+              slug
+              title
+            }
+            ... on DatoCmsPage {
+              id
+              slug
+              title
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const { menu } = data.allDatoCmsNavigation.nodes[0];
   const isDesktop = useMediaQuery('(min-width: 1024px)');
   const [isOpen, setOpen] = useState(false);
 
@@ -27,9 +51,10 @@ export default function Navigation({ menu }) {
       )}
       >
         <ul className={styles.list}>
-          { menu.map((link, index) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <li key={index} className={styles.link}><Link to={link.slug}>{`${link.name}.`}</Link></li>
+          { menu.map((link) => (
+            <li key={link.id} className={styles.link}>
+              <Link to={link.slug}>{`${link.title}.`}</Link>
+            </li>
           ))}
         </ul>
       </div>
@@ -37,13 +62,13 @@ export default function Navigation({ menu }) {
   );
 }
 
-Navigation.propTypes = {
-  menu: PropTypes.arrayOf(PropTypes.object),
-};
+// Navigation.propTypes = {
+//   menu: PropTypes.arrayOf(PropTypes.object),
+// };
 
-Navigation.defaultProps = {
-  menu: [
-    { slug: '/', name: 'Home' },
-    { slug: '/', name: 'About' },
-  ],
-};
+// Navigation.defaultProps = {
+//   menu: [
+//     { slug: '/', name: 'Home' },
+//     { slug: '/', name: 'About' },
+//   ],
+// };
