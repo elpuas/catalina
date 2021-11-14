1b6b2105eef13e47d9dc204aa16b1f3f4651f192
Alfredo Navas <elpuas@gmail.com>
2021-11-14 16:27:25 -0600

    image component

---
+++ src/components/bricks/Image/Image.js
@@ -0,0 +1,22 @@
+/* eslint-disable react/require-default-props */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { GatsbyImage } from 'gatsby-plugin-image';
+/**
+ * Render the image block component
+ *
+ * @param  {object}   props        The props object.
+ * @param {object}   props.image   The image model object
+ * @return {Element} The image block component
+ */
+export default function Image(props) {
+  const { image } = props;
+  return (
+    <GatsbyImage image={image.gatsbyImageData} alt={image.alt} />
+  );
+}
+
+Image.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  image: PropTypes.object,
+};
